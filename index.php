5400e17ed60e3ab53f9fd1153fad08cea2b9093b
G1tHub20 <a7m2cy@gmail.com>
2022-02-05 23:58:06 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+$dsn = 'mysql:dbname=mydb;host=localhost';
+$user = 'root';
+$password = 'mysqlpa55';
+
+try {
+  $pdo = new PDO($dsn, $user, $password);
+  echo 'DB接続成功';
+  $stmt = $pdo->prepare('SELECT * FROM memos LIMIT 7');
+  $stmt->execute();
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $data[] = $row;
+  }
+} catch (PDOException $e){
+  echo ($e->getMessage());
+  die();
+}
+// echo('<pre>');
+// var_dump($data);
+// echo('<pre>');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>メモ一覧</title>
+</head>
+<body>
+<table border="1">
+<?php foreach($data as $row): ?>
+  <tr>
+    <td><?php echo $row['id']; ?></td>
+    <td><?php echo $row['memo']; ?></td>
+    <td><?php echo $row['created']; ?></td>
+  </tr>
+<?php endforeach; ?>
+</table>
+
+</body>
+</html>
